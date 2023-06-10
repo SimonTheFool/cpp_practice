5ea11653620046de5147076108c0a2ec06c482aa
SimonTheFool <chengzilin2011@gmail.com>
2023-06-11 05:51:08 +0800

    Guess I looked at the wrong question. Sorry for making this kind of stupid mistake.

--- 5/5.2.cpp
+++ 5/5.2.cpp
@@ -1,20 +1,17 @@
 #include<iostream>
-
+#include<array>
 
 int main(){
-
-    int i;
-    for(i=0;i<5;i++){
-        std::cout<<i;
-        std::cout<<std::endl;
+    const int ArSize = 100;
+    std::array<long double,ArSize> factorials;
+    factorials[0]=factorials[1]=1;
+    std::cout<<0<<"! = "<<factorials[0]<<std::endl;
+    std::cout<<1<<"! = "<<factorials[1]<<std::endl;
+    for(int i = 2;i<=ArSize;i++){
+        factorials[i]=factorials[i-1]*i;
+        std::cout<<i<<"! = "<<factorials[i]<<std::endl;
     }
 
     return 0;
 }
-/*
-0
-1
-2
-3
-4
-*/
+// 100! = 9.33262e+157
